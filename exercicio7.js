da44e876f25468c318f893acbf13f4c88468da1f
Maria Vicente <mnvcaetano@gmail.com>
2023-07-16 16:01:44 -0300

    Exercício 7

--- exercicio7.js
+++ exercicio7.js
@@ -10,3 +10,26 @@
  *      remover os elementos do array compreendidos entre os índices inicial e final. Em seguida, retorna o
  *      array atualizado.
  */
+
+function adicionarElemento(array, elemento) {
+    array.push(elemento);
+    return array;
+  }
+  
+  function removerUltimoElemento(array) {
+    var elementoRemovido = array.pop();
+    return elementoRemovido;
+  }
+  
+  function removerElementosIntervalo(array, indiceInicial, indiceFinal) {
+    array.splice(indiceInicial, indiceFinal - indiceInicial + 1);
+    return array;
+  }
+  
+  var meuArray = [1, 2, 3, 4, 5];
+  
+  console.log(adicionarElemento(meuArray, 6));
+  
+  console.log(removerUltimoElemento(meuArray));
+  
+  console.log(removerElementosIntervalo(meuArray, 1, 3));
